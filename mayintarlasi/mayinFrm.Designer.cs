9da76e15ade6bc1e5884fd39b0210a86708569fc
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-12 15:24:16 +0300

    Proje dosyası ekle.

---
+++ mayintarlasi/mayinFrm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace mayintarlasi
+{
+    partial class mayinFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.baslabtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(62, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 274);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // baslabtn
+            // 
+            this.baslabtn.Location = new System.Drawing.Point(165, 26);
+            this.baslabtn.Name = "baslabtn";
+            this.baslabtn.Size = new System.Drawing.Size(80, 32);
+            this.baslabtn.TabIndex = 2;
+            this.baslabtn.Text = "Başla";
+            this.baslabtn.UseVisualStyleBackColor = true;
+            this.baslabtn.Click += new System.EventHandler(this.baslabtn_Click);
+            // 
+            // mayinFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 414);
+            this.Controls.Add(this.baslabtn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "mayinFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası V1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button baslabtn;
+    }
+}
+
---
+++ mayintarlasi/mayinFrm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayintarlasi
+{
+    public partial class mayinFrm : Form
+    {
+        Button[,] butonlar = new Button[5, 5];
+        int[,] oyunalani = new int[5, 5];
+        public mayinFrm()
+        {
+            InitializeComponent();
+        }
+
+        public int mayinvarmi(int x,int y)
+        {
+            int sayac = 0;
+            if (x < 4)
+            {
+                if (oyunalani[x + 1, y] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (x < 4 && y < 4)
+            {
+                if (oyunalani[x + 1, y + 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (y < 4)
+            {
+                if (oyunalani[x, y + 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (x > 0 && y < 4)
+            {
+                if (oyunalani[x - 1, y + 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (x > 0)
+            {
+                if (oyunalani[x - 1, y] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (x > 0 && y > 0)
+            {
+                if (oyunalani[x - 1, y - 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (y > 0)
+            {
+                if (oyunalani[x, y - 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            if (x < 4 && y > 0)
+            {
+                if (oyunalani[x + 1, y - 1] == 1)
+                {
+                    sayac++;
+                }
+            }
+            return sayac;
+        }
+
+        private void baslabtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (butonlar[i, j] != null)
+                    {
+                        butonlar[i, j].BackgroundImage = null;
+                        butonlar[i, j].Parent = null;
+                        butonlar[i,j]=null;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    oyunalani[i,j] = 0;
+                }
+            }
+            Random r=new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int x = r.Next(5);
+                int y = r.Next(5);
+                oyunalani[x, y] = 1;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {                          
+                    Button b = new Button();
+                    b.Width = 40;
+                    b.Height = 40;
+                    b.Left = 10+j*40;
+                    b.Top = 10+i*40;
+                    b.Name = i + "," + j;
+                    b.Click += oyunbtn_Click;
+                    b.Font = new Font("Arial", 14);
+                    b.Parent = groupBox1;
+                    butonlar[i,j] = b;
+                }
+            }            
+        }
+        private void oyunbtn_Click(object sender, EventArgs e)
+        {            
+            Button m=(Button)sender;
+            String[]dizi=m.Name.Split(',');
+            int x =int.Parse( dizi[0]);
+            int y = int.Parse(dizi[1]);
+            butonlar[x,y].BackColor=Color.Blue;
+            if (oyunalani[x,y]==1)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if(oyunalani[i, j] == 1)
+                        {
+                            butonlar[i,j].BackgroundImageLayout = ImageLayout.Stretch;
+                            butonlar[i, j].BackgroundImage = Image.FromFile("mine.png");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                butonlar[x, y].Text = mayinvarmi(x, y).ToString();
+            }
+        }
+    }
+}
